bad7bc9adaca873b2c8497f7475e61622501c8be
Folafusi <117306386+Folafusi@users.noreply.github.com>
2022-12-01 09:20:57 +0100

    Add files via upload

---
+++ LUKASISGIGGANH/LUKASISGIGGANH/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jj
+{
+	class PL
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("guess number");
+			float num = float.Parse(Console.ReadLine());
+            {
+				
+				if (num == 69)
+				{
+					Console.WriteLine("The number is fine");
+				}
+				if (num >= 2 && num <= 999999)
+				{
+					Console.WriteLine("Wrong number better next time");
+					Console.WriteLine("Try agian");
+					
+                }
+            }
+			Console.WriteLine("choose what you are");
+            string Thing = Console.ReadLine();
+			Console.WriteLine("Now Who");
+            string name = Console.ReadLine();
+            {
+				
+				Console.WriteLine("You are {0} {1}", Thing, name);
+            }
+		}
+	}
+}
+
+
+
---
+++ Lukas_je_blbej_vole/Lukas_je_blbej_vole/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ggo
+{
+    class f
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("number");
+            int a = int.Parse(Console.ReadLine());
+            int vysledek = a;
+            for (int i = 1; i <= 10; i++) ;
+            vysledek = vysledek * a;
+            Console.WriteLine("vysledek: {0} ", vysledek);
+            Console.ReadKey();
+        }
+    }
+}
